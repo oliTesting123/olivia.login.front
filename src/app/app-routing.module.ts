073fba1fe4651aa7b543dc678937feec20a3db3b
oliTesting123 <oiviasuarez83@gmail.com>
2023-10-31 14:45:36 -0600

    Added esign to login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DashboardComponent } from './modules/users/views/dashboard/dashboard.component';
+import { LoginComponent } from './modules/login/views/login/login.component';
 import { EditUserComponent } from './modules/users/components/edit-user/edit-user.component';
 
 const routes: Routes = [
   
   {
-    path: '', component: DashboardComponent
+    path: '', component: LoginComponent
   },
   {
     path: 'edit', component: EditUserComponent,
--- src/app/modules/modules.module.ts
+++ src/app/modules/modules.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ModulesRoutingModule } from './modules-routing.module';
+import { LoginComponent } from './login/views/login/login.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoginComponent
+  ],
   imports: [
     CommonModule,
     ModulesRoutingModule
